379c038fd54192f305992a1c4654d15e7dddc540
Gabriel Isper <2411600431@fema.edu.br>
2025-09-04 16:39:34 -0300

    initial commit

---
+++ src/test/java/spring/app/loopy/LoopyApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.app.loopy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoopyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
